10620a83c460c4da5ccffd224e1be13c2ebe01b5
Anni-Bessie Kitt <annibessie.kitt@gmail.com>
2017-01-08 13:08:02 +0200

    viimased muudatused

--- Andmebaas.java
+++ Andmebaas.java
@@ -47,16 +47,9 @@ public class Andmebaas {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        //}
-        //public void lisaDeployment (String kn, String tiernr, String en, String pn, String posits, String rahv, String alguskp, String loppkuupaev, Integer paevi, Integer fee ){
-        //   String sql = " INSERT INTO USERS (KN, TIERNR, EN, PN, POSITS, RAHV, ALGUSKP, LOPPKUUAEV, PAEVI, FEE) VALUES ('''+kn+''', '''+tiernr+''', )";
-        //    teeAndmebaasiMuudatus(sql);
-        //}
+
     }
-    //public void registreeriDeployment (ChoiceBox kn, ChoiceBox tiernr, TextField en, TextField pn, ChoiceBox posits, TextField rahv, DatePicker alguskp, DatePicker loppkuupaev, int paevi, int fee){
-    //    String sql = " INSERT INTO USERS (KN, TIERNR, EN, PN, POSITS, RAHV, ALGUSKP, LOPPKUUAEV, PAEVI, FEE) VALUES (""+kn+"", ""+tiernr+"", )";
-    //    teeAndmebaasiMuudatus(sql);
-    //}
+
     //Lisan andmebaasi tulpade järgi vastava info, mida soovin salvestada
     public void registreeriDeployment(String klient, String tiernumber, String inimeseeesnimi, String perekonnanimi, String position, String nationality, String algus, String lopp, String arvpaevi, String kliendifee) {
         String sql = " INSERT INTO DEPLOYMENTS (KLIENT, TIERNUMBER, INIMESEEESNIMI, PEREKONNANIMI, POSITSIOON, RAHVUS, ALGUS, LOPP, ARVPAEVI, KLIENDIFEE) VALUES ('"+klient+"', '"+tiernumber+"', '"+inimeseeesnimi+"', '"+perekonnanimi+"', '"+position+"', '"+nationality+"', '"+algus+"', '"+lopp+"', '"+arvpaevi+"', '"+kliendifee+"' )";
